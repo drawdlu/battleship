9ba6b1bee985166a1f6ef70449e4a50cc1b458c2
drawdlu <152858099+drawdlu@users.noreply.github.com>
2026-05-18 23:57:13 +0800

    feat: Prevent placing ships on invalid places
    When a part of the ship is beyond the board or when it is overlapping
    another ship

--- src/ui/ships.js
+++ src/ui/ships.js
@@ -37,8 +37,6 @@ function moveShip(e) {
   newX = startX - e.clientX;
   newY = startY - e.clientY;
 
-  console.log(window.scrollY);
-
   startX = e.clientX;
   startY = e.clientY;
 
@@ -50,40 +48,109 @@ function moveShip(e) {
   updateClosestElement(newShipY, newShipX);
 }
 
-function updateClosestElement(newShipY, newShipX) {
-  const boardDiv = document.querySelector(".player.board");
-  const position = getShipPositionFromWindow(newShipY, newShipX);
-  closestElement = document.elementFromPoint(
-    position.x - window.scrollX,
-    position.y - window.scrollY,
-  );
-}
-
 // 20 is just a little less than half the cell size
 // Makes placing the ships more intuitive than snapping it to the closest
 // cell on the top left corner of the ship position on "mouseup"
+function updateClosestElement(newShipY, newShipX) {
+  const boardDiv = document.querySelector(".player.board");
+  const position = getShipPositionFromWindow(newShipY, newShipX);
+  closestElement = document.elementFromPoint(position.x + 20, position.y + 20);
+}
+
 function getShipPositionFromWindow(newShipY, newShipX) {
   const board = document.querySelector(".player.board");
-  const y = board.offsetTop + newShipY;
-  const x = board.offsetLeft + newShipX;
+  const y = board.offsetTop + newShipY - window.scrollY;
+  const x = board.offsetLeft + newShipX - window.scrollX;
 
-  return { y: y + 20, x: x + 20 };
+  return { y: y, x: x };
 }
 
 function snapShipOnBoard(e) {
-  currentShip.classList.remove("dragging");
   document.removeEventListener("mousemove", moveShip);
   document.removeEventListener("mouseup", snapShipOnBoard);
 
-  // prevents errors when element is dragged past the window
-  const cell = closestElement ? closestElement.closest(".player .cell") : null;
+  const cell = returnClosestPlayerCell(closestElement);
 
-  if (cell) {
+  if (cell && checkIfMoveValid()) {
     moveShipToCellPosition(cell);
   } else {
     moveBackToOriginalPosition();
   }
+  currentShip.classList.remove("dragging");
 }
+
+function checkIfMoveValid(cell) {
+  const size = getShipSize();
+  let currentDiv = closestElement;
+  const add = getDimensionsToAdd();
+
+  for (let i = 1; i < size; ++i) {
+    const divPos = getElementPosition(currentDiv);
+    const nextCell = document.elementFromPoint(
+      divPos.left + add.left,
+      divPos.top + add.top,
+    );
+
+    if (!isAPlayerCell(nextCell)) {
+      return false;
+    }
+    currentDiv = nextCell;
+  }
+
+  return true;
+}
+
+// 20 here has same function as updateClosestElement only this time making sure
+// we are pointing close to the middle of the cell to capture wether it is
+// a ship or a cell if within board
+function getDimensionsToAdd() {
+  const cellSize = 50;
+  const vertical = shipClassContains("vertical");
+  const left = vertical ? 20 : cellSize + 20;
+  const top = vertical ? cellSize + 20 : 20;
+
+  return { left: left, top: top };
+}
+
+function getElementPosition(div) {
+  return div.getBoundingClientRect();
+}
+
+function isAPlayerCell(div) {
+  const playerBoard = document.querySelector(".player.board");
+
+  return playerBoard.contains(div) && div.classList.contains("cell");
+}
+
+function getShipSize() {
+  if (shipClassContains("two")) {
+    return 2;
+  } else if (shipClassContains("three")) {
+    return 3;
+  } else if (shipClassContains("four")) {
+    return 4;
+  } else if (shipClassContains("five")) {
+    return 5;
+  }
+}
+
+function shipClassContains(string) {
+  const classListString = currentShip.classList.value;
+  const regexp = new RegExp(`${string}`);
+
+  return regexp.test(classListString);
+}
+
+function returnClosestPlayerCell(div) {
+  // prevents errors when element is dragged past the window
+  if (!div) {
+    return null;
+  }
+
+  return div.closest(".player .cell");
+}
+
+function checkOverflowAndCollision(closestElement) {}
 
 function moveShipToCellPosition(cell) {
   const y = cell.offsetTop;
